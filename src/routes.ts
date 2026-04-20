1bd251a8cad747c422af7f9b7661376a6098e5a3
Danilo Marques <danilogustavomarques@gmail.com>
2026-04-20 10:16:08 -0300

    fix: commit apos git stash

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+<<<<<<< Updated upstream
 import { createUserController } from "./controllers/user/CreateUserController";
+=======
+import { createUserController } from "./controllers/users/CreateUserController";
+import { validateSchema } from "./middlewares/validateSchema";
+import { createUserSchema } from "./schemas/userSchema";
+>>>>>>> Stashed changes
 
 const router = Router();
 
